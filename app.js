a0057988d47ca57128fcd275c9f99a140371a9ae
j-img <imaginaris144@protonmail.com>
2020-10-18 20:22:59 +0200

    Orders

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 /* TODO
 
-Handling orders
+Order proceed
 Style
-Fix crash on /category/<unexisting route>
-Stats on overview page
 */
 
 
@@ -20,8 +18,9 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'DATABASE';
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var dev_db_url = 'mongodb+srv://dbUser:Q0Hpaoe8QEmvViLO@cluster0.auuk9.azure.mongodb.net/tea_shop?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || 'mongodb+srv://dbUser:Q0Hpaoe8QEmvViLO@cluster0.auuk9.azure.mongodb.net/tea_shop?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
@@ -39,12 +38,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
